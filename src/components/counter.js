b392012f3ecaa92deeb26deabc90ced9f5a43c5f
Bennie Sowah Jr <bensowahjr@gmail.com>
2022-11-26 10:51:29 -0600

    intial commit

---
+++ src/components/counter.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./counter.css"
+
+const Counter = (props) => {
+    const [count, setCount] = useState(0)
+
+    console.log(props);
+
+    const increase = () => {
+        setCount(count + props.value)
+        console.log(count);
+    }
+
+    const decrease = () => {
+        setCount(count - props.value)
+        console.log(count)
+    }
+    
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increase} className="button">+ {props.value}</button>
+            <button onClick={decrease} className="button">- {props.value}</button>
+        </div>
+    )
+}
+
+export default Counter
